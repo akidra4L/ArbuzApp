cb933d84c8ae944be5314b44d0cbf1d35072bf2e
Alikhan <jonasofan.gubaev@gmail.com>
2023-05-20 14:09:36 +0600

    initial commit

---
+++ ArbuzApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ArbuzApp
+//
+//  Created by Alikhan Gubayev on 20.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        return true
+    }
+
+}
+
---
+++ ArbuzApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArbuzApp
+//
+//  Created by Alikhan Gubayev on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
